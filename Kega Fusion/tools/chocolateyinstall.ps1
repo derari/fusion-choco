aac7ec4b6da53242622a433a4a256ffd7261a298
Arian Treffer <derari@web.de>
2015-06-01 14:20:29 +0200

    First version of installer

---
+++ Kega Fusion/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'Kega Fusion'
+$url = 'http://www.carpeludum.com/download/Fusion364.zip'
+
+$binRoot = Get-BinRoot
+$installDir = Join-Path $binRoot "$packageName"
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
+
+#Install-ChocolateyShortcut -shortcutFilePath "<path>" -targetPath "<path>"
